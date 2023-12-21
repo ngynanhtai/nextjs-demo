9f4628258f2cd2c506b1a8ba380636f8c671f38f
ngynanhtai <ngynanhtai@gmail.com>
2023-12-21 10:48:39 +0700

    completed nextjs instruction

---
+++ app/login/page.tsx
@@ -0,0 +1,22 @@
+import AcmeLogo from '../ui/acme-logo';
+import LoginForm from '../ui/login-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
+
+export default function Page() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="md: relative mx-auto -mt-32 flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
+}
